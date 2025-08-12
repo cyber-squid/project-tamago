d2bcedba220114127ca268e6da472566f2294b03
seapuff <smalbraiki94@gmail.com>
2025-08-12 19:53:36 +0400

    just taking down some notes for ideas on how to make the dialogue system

--- Project_Tamago/Assets/Scripts/Activities/Talk/TalkActivity.cs
+++ Project_Tamago/Assets/Scripts/Activities/Talk/TalkActivity.cs
@@ -9,7 +9,7 @@ public class TalkActivity : GenericActivity
 {
     // if the button is clicked on, it should prompt the tama to talk. 
     // this should create a query that uses all the currently loaded rules to determine the dialogue lines.
-    
+
     //
 
     // the response, along with prompting that particular dialogue, should then nullify the context data relevant to that dialogue since it's been said, but leave other data alone so
@@ -19,8 +19,15 @@ public class TalkActivity : GenericActivity
 
     // in certain cases, we'd want the game to force a talk, eg if something calls for checking a query that would prompt the force talk if it passes.
 
+    TalkDecisionHandler talkDecisionHandler;
+
     internal override void ChangeScreen()
     {
+        // should probably calculate, or retreive current query somewhere here?
+        if (talkDecisionHandler.dialogueDict.ContainsKey("testtext"))
+        {
+            
+        }
         mainScreen.SetActive(true);
     }
 
--- Project_Tamago/Assets/Scripts/Activities/Talk/TalkHandler.cs
+++ Project_Tamago/Assets/Scripts/Activities/Talk/TalkHandler.cs
@@ -4,27 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class TalkHandler
+
+// giant set of "rules" aka dialogues, (one for each personality?), and criteria that must be met to meet them.
+// represented by a dictionary, with the keys being criteria, and the values being the dialogues.
+// the criteria represent a wide array of data about the game state. energy, last game played, how mamy times you discussed today, etc.
+
+// perhaps criteria can be a class that has a massive, premade list of variables representing current state (probably its own dictionary?), 
+// with the criteria keys in the dictionary being instances that collectively represent all the different kinds of states the game could possibly be in?
+// and the query is a criteria class instance, whose values have been updated over the course of the game? then, when you want to talk, 
+// the query is (maybe turned into a string first? idk) used to check for the criteria class with all the same values, and the dialogue value paired 
+// to that gets passed to the talkactivity.
+//
+// for an event that your tama forces an interaction, we could set a flag somewhere in talkactivity to activate, which in turn 
+// automatically compares the query to the dictionary. 
+
+
+// to poll for a line from the dictionary, we can concatenate together all the values for all the criteria into one big string. 
+// if this string matches a key in the dictionary, we can pass the dialogue value paired to it to the talkactivity, for it to display. 
+
+
+public class TalkDecisionHandler
 {
     public Dictionary<string, Dialogue> dialogueDict; // = new Dictionary<string, Dialogue>();
 
+    
 
+    public TalkDecisionHandler() 
+    {
+        string[] loadedText = new string[2];
+        loadedText[0] = "hi";
+        loadedText[1] = "its me";
+        dialogueDict.Add("testtext", new Dialogue(loadedText));
+    }
     /*
      * queryhandler 
      */
 
 
 
-
+    public Dialogue DetermineDialogue(string key)
+    {
+        return dialogueDict[key];
+    }
 
 
 
 }
 
+// ***** IMPORTANT!!! REMEMBER, THINK ABOUT HOW TO IMPLEMENT CHOICE DIALOGUES LATER. *****
+// for now, just try to get a piece of dialogue from the dialogue class to display. 
 public class Dialogue
 {
-    string[] linesToSay;
+    public string[] linesToSay;
     
+    public Dialogue(string[] linesToSay)
+    {
+        this.linesToSay = linesToSay;
+    }
 
     public virtual void OnSaid() { }
 
@@ -34,3 +70,11 @@ public class Query
 {
     List<string> criteria;
 }
+
+/*
+ * some potential values:
+ * 
+ * numberOfTimesSpokenToInLifetime
+ * numberOfTimesSpokenToInAnHour
+ * numberOfTimesSpokenToInARow
+ */
